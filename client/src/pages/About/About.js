962116dcd3d61410ce7b3ccd1190d77f8d106c1d
Mthao09 <mthaoqk@users.noreply.github.com>
2018-08-22 11:47:38 -0500

    Bug fixes, fixed dummy to smart components

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,60 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
 import Hero from "../../components/Hero";
 import "./About.css";
 import pic1 from "./entrepreneur.jpg";
-import pic2 from "./angelinvestor-300x300.jpg";
-const About = () => (
-  <div>
-    <Hero backgroundImage="https://s8.postimg.cc/aqr93z6lx/test.jpg">
-      <h1>GoPUBLk</h1>
-    </Hero>
+import pic2 from "./angelInvestor.jpg";
 
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12">
-          <div className="jumbotron">
-            <h1>The Mission of GoPUBLk</h1>
-            <p>Connecting Entrepreneurs and Angel Investors. Fostering Relationships, Effecting Opportunity.
-              <hr></hr>
-              </p>
-          </div>
+class About extends Component {
+
+
+
+
+  
+  render() {
+    return (
+      <div>
+        <Hero backgroundImage="https://s8.postimg.cc/aqr93z6lx/test.jpg">
+          <h1>GoPUBLk</h1>
+        </Hero>
+
+        <div className="container">
           <div className="row">
-            <div className="col-md-6">
-              <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src={pic1} alt="Card image cap"></img>
-                  <div class="card-body">
-                  <h5 class="card-title">Benefits For Entrepreneurs</h5>
-                  <ul>
-                    <li>High Exposure</li>
-                    <li>Diverse Investor Backgrounds</li>
-                    <li>Mentorship</li>
-                    <li>Low Barrier to Entry</li>
-                  </ul>
-                  <a href="/create" class="btn btn-primary">Submit Your Project</a>
+            <div className="col-md-12">
+              <div className="jumbotron">
+                <h1>The Mission of GoPUBLk</h1>
+                <p>Connecting Entrepreneurs and Angel Investors. Fostering Relationships, Effecting Opportunity.
+                <hr></hr>
+                </p>
+              </div>
+              <div className="row">
+                <div className="col-md-6">
+                  <div class="card"
+                  // style="width: 18rem;"
+                  >
+                    <img class="card-img-top" src={pic1} alt="En"></img>
+                    <div class="card-body">
+                      <h5 class="card-title">Benefits For Entrepreneurs</h5>
+                      <ul>
+                        <li>High Exposure</li>
+                        <li>Diverse Investor Backgrounds</li>
+                        <li>Mentorship</li>
+                        <li>Low Barrier to Entry</li>
+                      </ul>
+                      <a href="/create" class="btn btn-primary">Submit Your Project</a>
+                    </div>
                   </div>
                 </div>
-          </div>
-          <div className="col-md-6">
-          <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src={pic2} alt="Card image cap"></img>
-                  <div class="card-body">
-                  <h5 class="card-title">Benefits For Investors</h5>
-                  <ul>
-                    <li>Broad range of industries</li>
-                    <li>Highly detailed business plans</li>
-                    <li>Ease of use</li>
-                    <li>Low barrier to entry</li>
-                  </ul>
-                  <a href="/search" class="btn btn-primary">Browse Projects</a>
+                <div className="col-md-6">
+                  <div class="card"
+                  // style="width: 18rem;"
+                  >
+                    <img class="card-img-top" src={pic2} alt="Card image cap"></img>
+                    <div class="card-body">
+                      <h5 class="card-title">Benefits For Investors</h5>
+                      <ul>
+                        <li>Broad range of industries</li>
+                        <li>Highly detailed business plans</li>
+                        <li>Ease of use</li>
+                        <li>Low barrier to entry</li>
+                      </ul>
+                      <a href="/search" class="btn btn-primary">Browse Projects</a>
+                    </div>
                   </div>
                 </div>
-          </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-      
-);
+    )
+  }
+
+}
+
 
 export default About;
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,30 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import Hero from "../../components/Hero";
 import "./Profile.css";
 import ProfileNavBar from "../../components/ProfileNavBar/"
 
-const Profile = () => (
-  <div>
-    <Hero backgroundImage="https://s8.postimg.cc/aqr93z6lx/test.jpg">
-      <h1>GoPUBLk</h1>
-  </Hero>
-   
-    <div className="container">
-      <div className="row">
-      <div className="col-md-12">
-        <div className="jumbotron">
-        <p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out.
-           Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... 
-           and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that
-            killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <Hero backgroundImage="https://s8.postimg.cc/aqr93z6lx/test.jpg">
+          <h1>GoPUBLk</h1>
+        </Hero>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="jumbotron">
+                <p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out.
+                   Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide...
+                   and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that
+                    killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.
         </p>
-      
+
+              </div>
+              <ProfileNavBar />
+            </div>
           </div>
-          <ProfileNavBar/>
         </div>
       </div>
-    </div>
-  </div>
-);
+    )
+  }
+}
 
 export default Profile;
